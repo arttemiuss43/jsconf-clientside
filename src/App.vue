7c4de02b1a0c7464babc509f77c63be9b3f960e8
Nischuk Artyom <arttemiuss43@gmail.com>
2019-05-06 11:38:53 +0600

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <h1>Client Side Rendering</h1>
+    <h1>Hello {{ name }}</h1>
+    <button @click="changeName">Change name</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      name: 'world'
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Vasya'
+    }
+  }
 }
 </script>
